b70a9bfbedf5370c51612c3553527aa5192eaed1
Ali Mrhj <mrhj26ali@gmail.com>
2026-06-20 11:38:49 +0300

    feat(runtime): implement manual Arabic Reshaper and Naive Bidi engine in C

--- output.s
+++ output.s
@@ -1,26 +1,52 @@
-	.file	"tmpa2lanws2.ll"
+	.file	"tmplpne8ziq.ll"
 	.section	.ltext,"axl",@progbits
 	.globl	"تشغيل_النظام"
 	.p2align	4
 	.type	"تشغيل_النظام",@function
 "تشغيل_النظام":
+	.cfi_startproc
 	pushq	%rsi
-	subq	$32, %rsp
+	.cfi_def_cfa_offset 16
+	pushq	%rdi
+	.cfi_def_cfa_offset 24
+	subq	$40, %rsp
+	.cfi_def_cfa_offset 64
+	.cfi_offset %rdi, -24
+	.cfi_offset %rsi, -16
 	movabsq	$"العمر", %rax
 	movl	(%rax), %eax
 	incl	%eax
 	movabsq	$"السنة_القادمة", %rcx
 	movl	%eax, (%rcx)
-	movabsq	$.Lstr, %rcx
-	movabsq	$puts, %rsi
+	movabsq	$str_0, %rcx
+	movabsq	$print_arabic, %rsi
 	callq	*%rsi
-	movabsq	$.Lstr.1, %rcx
-	movq	%rsi, %rax
-	addq	$32, %rsp
+	movabsq	$putchar, %rdi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_2, %rcx
+	callq	*%rsi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_4, %rcx
+	callq	*%rsi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_6, %rcx
+	callq	*%rsi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_8, %rcx
+	callq	*%rsi
+	movl	$10, %ecx
+	movq	%rdi, %rax
+	addq	$40, %rsp
+	popq	%rdi
 	popq	%rsi
 	rex64 jmpq	*%rax
 .Lfunc_end0:
 	.size	"تشغيل_النظام", .Lfunc_end0-"تشغيل_النظام"
+	.cfi_endproc
 
 	.globl	main
 	.p2align	4
@@ -29,8 +55,11 @@ main:
 	.cfi_startproc
 	pushq	%rsi
 	.cfi_def_cfa_offset 16
-	subq	$32, %rsp
-	.cfi_def_cfa_offset 48
+	pushq	%rdi
+	.cfi_def_cfa_offset 24
+	subq	$40, %rsp
+	.cfi_def_cfa_offset 64
+	.cfi_offset %rdi, -24
 	.cfi_offset %rsi, -16
 	movabsq	$setup_arabic_console, %rax
 	callq	*%rax
@@ -38,13 +67,31 @@ main:
 	movl	$25, (%rax)
 	movabsq	$"السنة_القادمة", %rax
 	movl	$26, (%rax)
-	movabsq	$.Lstr, %rcx
-	movabsq	$puts, %rsi
+	movabsq	$str_0, %rcx
+	movabsq	$print_arabic, %rsi
 	callq	*%rsi
-	movabsq	$.Lstr.1, %rcx
+	movabsq	$putchar, %rdi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_2, %rcx
 	callq	*%rsi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_4, %rcx
+	callq	*%rsi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_6, %rcx
+	callq	*%rsi
+	movl	$10, %ecx
+	callq	*%rdi
+	movabsq	$str_8, %rcx
+	callq	*%rsi
+	movl	$10, %ecx
+	callq	*%rdi
 	xorl	%eax, %eax
-	addq	$32, %rsp
+	addq	$40, %rsp
+	popq	%rdi
 	popq	%rsi
 	retq
 .Lfunc_end1:
@@ -71,25 +118,65 @@ main:
 	.globl	str_0
 	.p2align	4, 0x0
 str_0:
-	.asciz	"[\330\263\330\254\331\204] \330\247\331\207\331\204\330\247 \330\250\331\203 \331\201\331\212 \331\204\330\272\330\252\331\203 \330\247\331\204\330\271\330\261\330\250\331\212\330\251 \330\247\331\204\330\256\330\247\330\265\330\251!\n"
-	.size	str_0, 67
+	.asciz	"[\330\263\330\254\331\204] \331\203\330\252\330\247\330\250\330\214 \330\250\330\247\330\250\330\247\330\214 \331\202\331\204\331\205."
+	.size	str_0, 39
 
 	.type	str_1,@object
 	.globl	str_1
-	.p2align	4, 0x0
 str_1:
-	.asciz	"[\330\263\330\254\331\204] \330\252\331\205 \330\255\330\263\330\247\330\250 \330\247\331\204\330\271\331\205\330\261 \330\250\331\206\330\254\330\247\330\255.\n"
-	.size	str_1, 47
+	.asciz	"\n"
+	.size	str_1, 2
 
-	.type	.Lstr,@object
-	.section	.lrodata.str1.1,"aMSl",@progbits,1
-.Lstr:
-	.asciz	"[\330\263\330\254\331\204] \330\247\331\207\331\204\330\247 \330\250\331\203 \331\201\331\212 \331\204\330\272\330\252\331\203 \330\247\331\204\330\271\330\261\330\250\331\212\330\251 \330\247\331\204\330\256\330\247\330\265\330\251!"
-	.size	.Lstr, 66
+	.type	str_2,@object
+	.globl	str_2
+	.p2align	4, 0x0
+str_2:
+	.asciz	"[\330\263\330\254\331\204] \331\202\330\247\331\204: \330\247\331\204\330\247\330\263\331\204\330\247\331\205\330\214 \330\247\331\204\331\204\331\207\330\214 \331\204\330\247\331\206."
+	.size	str_2, 53
 
-	.type	.Lstr.1,@object
-.Lstr.1:
+	.type	str_3,@object
+	.globl	str_3
+str_3:
+	.asciz	"\n"
+	.size	str_3, 2
+
+	.type	str_4,@object
+	.globl	str_4
+	.p2align	4, 0x0
+str_4:
+	.asciz	"[\330\263\330\254\331\204] \330\247\331\204\330\255\330\261\330\247\330\261\330\251 \330\247\331\204\331\212\331\210\331\205 25 \330\257\330\261\330\254\330\251 \331\205\330\246\331\210\331\212\330\251."
+	.size	str_4, 59
+
+	.type	str_5,@object
+	.globl	str_5
+str_5:
+	.asciz	"\n"
+	.size	str_5, 2
+
+	.type	str_6,@object
+	.globl	str_6
+	.p2align	4, 0x0
+str_6:
+	.asciz	"[\330\263\330\254\331\204] \330\247\331\204\330\247\330\265\330\257\330\247\330\261 v1.0 \331\205\331\206 \331\204\330\272\330\251 \331\205\331\212\330\262\330\247\331\206."
+	.size	str_6, 53
+
+	.type	str_7,@object
+	.globl	str_7
+str_7:
+	.asciz	"\n"
+	.size	str_7, 2
+
+	.type	str_8,@object
+	.globl	str_8
+	.p2align	4, 0x0
+str_8:
 	.asciz	"[\330\263\330\254\331\204] \330\252\331\205 \330\255\330\263\330\247\330\250 \330\247\331\204\330\271\331\205\330\261 \330\250\331\206\330\254\330\247\330\255."
-	.size	.Lstr.1, 46
+	.size	str_8, 46
+
+	.type	str_9,@object
+	.globl	str_9
+str_9:
+	.asciz	"\n"
+	.size	str_9, 2
 
 	.section	".note.GNU-stack","",@progbits
