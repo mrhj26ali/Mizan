912ff5b4e6d4f3b1e7a5e2882527c9474502320f
Ali Mrhj <mrhj26ali@gmail.com>
2026-06-20 10:09:09 +0300

    feat(backend): automate linking and generate native Windows executable

--- runtime.c
+++ runtime.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// هذه الدالة سيستدعيها مترجمنا عند اكتشاف محاولة قسمة على صفر
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+// دالة تهيئة يتم استدعاؤها آليًا لضبط شاشة ويندوز على UTF-8
+void setup_arabic_console() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+}
+
+// دالة الطوارئ التي يستدعيها مترجمنا عند القسمة على صفر
 void panic_div_zero() {
+    setup_arabic_console(); // نضمن أن رسالة الخطأ سُتطبع بشكل صحيح
     printf("\n [خطأ وقت التشغيل]: محاولة قسمة على صفر! تم إيقاف البرنامج بأمان \n");
-    exit(1); 
+    exit(1);
 }
