1c0dc320ec8fefec4bc3ccb58d4e4053919761c2
Ali Mrhj <mrhj26ali@gmail.com>
2026-06-24 07:16:30 +0300

    test 5 examples and did changing about printing

--- output.s
+++ output.s
@@ -1,25 +1,31 @@
 	.file	"<string>"
 	.section	.ltext,"axl",@progbits
-	.globl	"proc_الرئيسي"
+	.globl	"proc_حساب_السرعة"
 	.p2align	4
-	.type	"proc_الرئيسي",@function
-"proc_الرئيسي":
+	.type	"proc_حساب_السرعة",@function
+"proc_حساب_السرعة":
 	.cfi_startproc
-	pushq	%rsi
-	.cfi_def_cfa_offset 16
-	subq	$32, %rsp
-	.cfi_def_cfa_offset 48
-	.cfi_offset %rsi, -16
+	subq	$56, %rsp
+	movapd	%xmm6, 32(%rsp)
+	.cfi_def_cfa_offset 64
+	.cfi_offset %xmm6, -32
+	xorpd	%xmm6, %xmm6
+	ucomisd	%xmm1, %xmm6
+	jae	.LBB0_1
+	divsd	%xmm1, %xmm0
+	movapd	%xmm0, %xmm6
+	jmp	.LBB0_2
+.LBB0_1:
 	movabsq	$.L__log_1, %rcx
-	movabsq	$mizan_log, %rsi
-	callq	*%rsi
-	movabsq	$.L__log_2, %rcx
-	movq	%rsi, %rax
-	addq	$32, %rsp
-	popq	%rsi
-	rex64 jmpq	*%rax
+	movabsq	$mizan_log, %rax
+	callq	*%rax
+.LBB0_2:
+	movapd	%xmm6, %xmm0
+	movaps	32(%rsp), %xmm6
+	addq	$56, %rsp
+	retq
 .Lfunc_end0:
-	.size	"proc_الرئيسي", .Lfunc_end0-"proc_الرئيسي"
+	.size	"proc_حساب_السرعة", .Lfunc_end0-"proc_حساب_السرعة"
 	.cfi_endproc
 
 	.globl	main
@@ -43,14 +49,14 @@ main:
 	.cfi_offset %r14, -16
 	movabsq	$setup_arabic_console, %rax
 	callq	*%rax
-	movabsq	$.L__dev_ip_3, %rcx
+	movabsq	$.L__dev_ip_2, %rcx
 	movabsq	$mizan_modbus_connect, %rax
 	movl	$5020, %edx
 	callq	*%rax
 	movabsq	$__modbus_ctx, %rcx
 	movq	%rax, (%rcx)
-	movabsq	$.L__mq_host_4, %rcx
-	movabsq	$.L__mq_cid_5, %r8
+	movabsq	$.L__mq_host_3, %rcx
+	movabsq	$.L__mq_cid_4, %r8
 	movabsq	$mizan_mqtt_connect, %rax
 	movl	$1884, %edx
 	callq	*%rax
@@ -59,16 +65,33 @@ main:
 	movabsq	$__mizan_set_mqtt_ctx, %rdx
 	movq	%rax, %rcx
 	callq	*%rdx
-	movabsq	$.L__log_1, %rcx
-	movabsq	$mizan_log, %rsi
-	callq	*%rsi
-	movabsq	$.L__log_2, %rcx
-	callq	*%rsi
+	movabsq	$"g_عداد", %rdi
+	movl	$0, (%rdi)
+	movabsq	$.L__log_5, %rcx
+	movabsq	$mizan_log, %rbx
+	callq	*%rbx
+	movabsq	$.L__alert_6, %rdx
+	movabsq	$mizan_alert, %rax
+	movl	$1, %ecx
+	callq	*%rax
+	cmpl	$1, (%rdi)
+	jg	.LBB1_3
+	movabsq	$.L__log_8, %rsi
+	.p2align	4
+.LBB1_2:
+	movq	%rsi, %rcx
+	callq	*%rbx
+	movl	(%rdi), %eax
+	incl	%eax
+	movl	%eax, (%rdi)
+	cmpl	$2, %eax
+	jl	.LBB1_2
+.LBB1_3:
 	movabsq	$mizan_now_ms, %rdi
 	movabsq	$mizan_escalation_tick, %rbx
 	movabsq	$mizan_sleep_ms, %r14
 	.p2align	4
-.LBB1_1:
+.LBB1_4:
 	callq	*%rdi
 	movq	%rax, %rsi
 	callq	*%rbx
@@ -76,16 +99,22 @@ main:
 	subq	%rax, %rsi
 	addq	$1000, %rsi
 	testq	%rsi, %rsi
-	jle	.LBB1_1
+	jle	.LBB1_4
 	movq	%rsi, %rcx
 	callq	*%r14
-	jmp	.LBB1_1
+	jmp	.LBB1_4
 .Lfunc_end1:
 	.size	main, .Lfunc_end1-main
 	.cfi_endproc
 
-	.type	__modbus_ctx,@object
+	.type	"g_عداد",@object
 	.section	.lbss,"awl",@nobits
+	.p2align	2, 0x0
+"g_عداد":
+	.long	0
+	.size	"g_عداد", 4
+
+	.type	__modbus_ctx,@object
 	.p2align	3, 0x0
 __modbus_ctx:
 	.quad	0
@@ -101,28 +130,40 @@ __mqtt_ctx:
 	.section	.lrodata,"al",@progbits
 	.p2align	4, 0x0
 .L__log_1:
-	.asciz	"[\330\263\330\254\331\204] \331\205\330\261\330\255\330\250\330\247 \330\250\330\247\331\204\330\271\330\247\331\204\331\205 \331\205\331\206 \331\204\330\272\330\251 \331\205\331\212\330\262\330\247\331\206"
-	.size	.L__log_1, 58
+	.asciz	"\330\256\330\267\330\247: \330\247\331\204\330\262\331\205\331\206 \331\212\330\254\330\250 \330\247\331\206 \331\212\331\203\331\210\331\206 \330\247\331\203\330\250\330\261 \331\205\331\206 \330\265\331\201\330\261"
+	.size	.L__log_1, 61
 
-	.type	.L__log_2,@object
-	.p2align	4, 0x0
-.L__log_2:
-	.asciz	"[\330\263\330\254\331\204] \331\207\330\260\331\207 \331\204\330\272\330\251 \330\250\330\261\331\205\330\254\330\251 \330\271\330\261\330\250\331\212\330\251 \331\205\330\254\331\205\330\271\330\251"
-	.size	.L__log_2, 56
-
-	.type	.L__dev_ip_3,@object
-.L__dev_ip_3:
+	.type	.L__dev_ip_2,@object
+.L__dev_ip_2:
 	.asciz	"127.0.0.1"
-	.size	.L__dev_ip_3, 10
+	.size	.L__dev_ip_2, 10
 
-	.type	.L__mq_host_4,@object
-.L__mq_host_4:
+	.type	.L__mq_host_3,@object
+.L__mq_host_3:
 	.asciz	"127.0.0.1"
-	.size	.L__mq_host_4, 10
+	.size	.L__mq_host_3, 10
 
-	.type	.L__mq_cid_5,@object
-.L__mq_cid_5:
+	.type	.L__mq_cid_4,@object
+.L__mq_cid_4:
 	.asciz	"mizan-runtime"
-	.size	.L__mq_cid_5, 14
+	.size	.L__mq_cid_4, 14
+
+	.type	.L__log_5,@object
+	.p2align	4, 0x0
+.L__log_5:
+	.asciz	"\330\250\330\257\330\241 \330\247\330\256\330\252\330\250\330\247\330\261 \330\247\331\204\331\210\330\255\330\257\330\247\330\252"
+	.size	.L__log_5, 35
+
+	.type	.L__alert_6,@object
+	.p2align	4, 0x0
+.L__alert_6:
+	.asciz	"[\330\252\331\206\330\250\331\212\331\207 \331\205\330\263\330\252\331\210\331\211_1] \330\247\331\204\330\263\330\261\330\271\330\251 \330\271\330\247\331\204\331\212\330\251"
+	.size	.L__alert_6, 50
+
+	.type	.L__log_8,@object
+	.p2align	4, 0x0
+.L__log_8:
+	.asciz	"\330\257\331\210\330\261\330\251 \330\255\330\263\330\247\330\250\331\212\330\251"
+	.size	.L__log_8, 22
 
 	.section	".note.GNU-stack","",@progbits
--- runtime/mizan_mqtt.c
+++ runtime/mizan_mqtt.c
@@ -5,8 +5,8 @@
 #include <string.h>
 
 typedef struct { struct mosquitto* m; int connected; } MQCtx;
-
 MQCtx* _g_mqtt = NULL;
+
 void __mizan_set_mqtt_ctx(void* ctx) { _g_mqtt = (MQCtx*)ctx; }
 
 static void _on_connect(struct mosquitto* m, void* obj, int rc) {
@@ -15,6 +15,7 @@ static void _on_connect(struct mosquitto* m, void* obj, int rc) {
     if (rc == 0) printf("[MQTT] ✅ اتصال ناجح بالوسيط\n");
     else fprintf(stderr,"[MQTT] ⚠️ فشل الاتصال (rc=%d)\n", rc);
 }
+
 static void _on_disconnect(struct mosquitto* m, void* obj, int rc) {
     ((MQCtx*)obj)->connected = 0;
     fprintf(stderr,"[MQTT] ⚠️ انقطع الاتصال\n");
@@ -25,16 +26,21 @@ void* mizan_mqtt_connect(const char* host, int port, const char* cid) {
     const char* ep = getenv("MIZAN_MQTT_PORT");
     if (eh) host = eh;
     if (ep) port = atoi(ep);
+
     static int lib_init = 0;
     if (!lib_init) { mosquitto_lib_init(); lib_init = 1; }
+
     MQCtx* c = (MQCtx*)calloc(1, sizeof(MQCtx));
     if (!c) return NULL;
+
     c->m = mosquitto_new(cid, true, c);
     if (!c->m) { free(c); return NULL; }
+
     mosquitto_connect_callback_set(c->m, _on_connect);
     mosquitto_disconnect_callback_set(c->m, _on_disconnect);
     mosquitto_connect(c->m, host, port, 60);
     mosquitto_loop_start(c->m);
+
     return c;
 }
 
